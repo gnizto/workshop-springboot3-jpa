568e3ab671b1743d92bb18cc65f8f9a2b52a9129
gnizto <gnizto@outlook.com>
2025-01-08 21:30:56 -0300

    OrderStatus enum

--- src/main/java/com/gabrieldm/course/entities/Order.java
+++ src/main/java/com/gabrieldm/course/entities/Order.java
@@ -25,6 +25,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -32,10 +34,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -55,14 +58,24 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
-	public User getClient() {
-		return client;
-	}
-
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
 
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+	
+	public User getClient() {
+		return client;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
