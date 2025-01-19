2f61e798bfef3c7c30e451173e8efbf682dd25a1
gnizto <gnizto@outlook.com>
2025-01-19 11:38:43 -0300

    Exception handling - update

--- src/main/java/com/gabrieldm/course/services/UserService.java
+++ src/main/java/com/gabrieldm/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.gabrieldm.course.repositories.UserRepository;
 import com.gabrieldm.course.services.exceptions.DatabaseException;
 import com.gabrieldm.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -41,9 +43,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 
